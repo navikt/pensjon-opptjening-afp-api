f0c1a25eed58c9de94c1b29791d7363986b292bd
Jan Kroken <jan.kroken@nav.no>
2024-02-11 23:47:16 +0100

    endring på requestlogging (for å kartlegge hva som egentlig bør logges)

--- src/main/kotlin/no/nav/pensjon/opptjening/afp/api/api/filter/LoggingInterceptor.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/afp/api/api/filter/LoggingInterceptor.kt
@@ -10,6 +10,7 @@ import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
+import java.util.*
 
 
 @Order(value = Ordered.HIGHEST_PRECEDENCE)
@@ -39,13 +40,25 @@ class LoggingInterceptor : HandlerInterceptor {
 
     inner class RequestResponseLogger {
         private val log: Logger = LoggerFactory.getLogger(this::class.java)
+        private val secureLog: Logger = LoggerFactory.getLogger("secure")
 
         fun logRequest(request: HttpServletRequest) {
+            val corr = UUID.randomUUID().toString()
             log.info("Request: ${request.method}, url: ${request.requestURL}, host: ${request.remoteHost}")
+            val headerNames = request.headerNames.toList().joinToString(",")
+            secureLog.info("Correlation id: $corr (for incoming request logging only)")
+            secureLog.info("Request($corr): ${request.method}, url: ${request.requestURL}, host: ${request.remoteHost}")
+            secureLog.debug("Request($corr) header names: $headerNames")
+
         }
 
         fun logResponse(response: HttpServletResponse) {
+            val corr = UUID.randomUUID().toString()
+            val headerNames = response.headerNames.toList().joinToString(",")
             log.info("Response: ${response.status}")
+            secureLog.info("Correlation id: $corr (for outgoing response only)")
+            secureLog.info("Response ($corr) status: ${response.status} content type:${response.contentType}")
+            secureLog.info("Response ($corr) headers: $headerNames")
         }
     }
 }
