d46c7506adffe39666140846a7675e871c15184d
Jacob Meidell <jacob.meidell@nav.no>
2024-02-21 15:40:43 +0100

    Experiment with logging 2

--- src/main/kotlin/no/nav/pensjon/opptjening/afp/api/api/filter/LoggingInterceptor.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/afp/api/api/filter/LoggingInterceptor.kt
@@ -1,15 +1,21 @@
 package no.nav.pensjon.opptjening.afp.api.api.filter
 
+import com.nimbusds.jwt.JWTParser
 import jakarta.servlet.annotation.WebFilter
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import net.logstash.logback.marker.Markers
+import net.minidev.json.JSONObject
+import org.apache.commons.lang3.StringUtils.startsWith
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
+import java.time.Instant
 import java.util.*
 
 
@@ -18,13 +24,20 @@ import java.util.*
 @WebFilter(filterName = "RequestLoggingFilter", urlPatterns = ["/api/*"])
 class LoggingInterceptor : HandlerInterceptor {
 
-    private val logger: RequestResponseLogger = RequestResponseLogger()
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger("RequestLoggingFilter")
+    }
+
     override fun preHandle(
         request: HttpServletRequest,
         response: HttpServletResponse,
         handler: Any
     ): Boolean {
-        logger.logRequest(request)
+        val id = request.getHeader("Nav-Call-Id") ?: request.getHeader("x-request-id") ?: UUID.randomUUID().toString()
+
+        request.setAttribute("id", id)
+        request.setAttribute("startTime", Instant.now())
+
         return super.preHandle(request, response, handler)
     }
 
@@ -34,31 +47,50 @@ class LoggingInterceptor : HandlerInterceptor {
         handler: Any,
         ex: Exception?
     ) {
-        logger.logResponse(response)
+        val timestamp = Instant.now()
+        val authHeader = request.getHeader(HttpHeaders.AUTHORIZATION)
+        val tokenData = if (authHeader.startsWith("bearer ", ignoreCase = true)) {
+            try {
+                JWTParser.parse(authHeader.substring("bearer ".length)).jwtClaimsSet
+            } catch (e: RuntimeException) {
+                log.info("Unable to parse bearer token", e)
+                null
+            }
+        } else {
+            null
+        }
+
+        val logData = mapOf(
+            "id" to request.getAttribute("id"),
+            "version" to (System.getenv("NAIS_APP_IMAGE") ?: ""),
+            "method" to request.method,
+            "protocol" to request.protocol,
+            "statusCode" to response.status,
+            "requestedUrl" to request.requestURL.toString(),
+            "requestedUri" to request.requestURI,
+            "remoteHost" to request.remoteHost,
+            "contentLength" to request.contentLength.toString(),
+            "elapsedTime" to (timestamp.toEpochMilli() - (request.getAttribute("startTime") as Instant).toEpochMilli()).toString() + "ms",
+            "threadName" to Thread.currentThread().name,
+            "token_audience" to tokenData?.audience,
+            "token_azp_name" to tokenData?.getClaim("azp_name"),
+            "token_scope" to tokenData?.getClaim("scope"),
+            "token_issuer" to tokenData?.issuer,
+            "token_subject" to tokenData?.subject,
+        )
+
+        val markers = Markers.appendEntries(logData)
+
+        log.info(
+            markers,
+            "id: {}, {}, {}, {}, {}",
+            logData["id"],
+            logData["protocol"],
+            logData["requestedUri"],
+            logData["statusCode"],
+            logData["elapsedTime"]
+        )
+
         super.afterCompletion(request, response, handler, ex)
-    }
-
-    inner class RequestResponseLogger {
-        private val log: Logger = LoggerFactory.getLogger(this::class.java)
-        private val secureLog: Logger = LoggerFactory.getLogger("secure")
-
-        fun logRequest(request: HttpServletRequest) {
-            val corr = UUID.randomUUID().toString()
-            log.info("Request: ${request.method}, url: ${request.requestURL}, host: ${request.remoteHost}")
-            val headerNames = request.headerNames.toList().joinToString(",")
-            secureLog.info("Correlation id: $corr (for incoming request logging only)")
-            secureLog.info("Request($corr): ${request.method}, url: ${request.requestURL}, host: ${request.remoteHost}")
-            secureLog.debug("Request($corr) header names: $headerNames")
-
-        }
-
-        fun logResponse(response: HttpServletResponse) {
-            val corr = UUID.randomUUID().toString()
-            val headerNames = response.headerNames.toList().joinToString(",")
-            log.info("Response: ${response.status}")
-            secureLog.info("Correlation id: $corr (for outgoing response only)")
-            secureLog.info("Response ($corr) status: ${response.status} content type:${response.contentType}")
-            secureLog.info("Response ($corr) headers: $headerNames")
-        }
     }
 }
