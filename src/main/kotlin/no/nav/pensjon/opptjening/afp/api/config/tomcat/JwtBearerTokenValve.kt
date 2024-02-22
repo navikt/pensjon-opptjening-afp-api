ecbebeecfc5da8be85a68a6a0dc6ac184bc85b87
Jacob Meidell <jacob.meidell@nav.no>
2024-02-22 09:06:29 +0100

    Add consumer and roles if present

--- src/main/kotlin/no/nav/pensjon/opptjening/afp/api/config/tomcat/JwtBearerTokenValve.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/afp/api/config/tomcat/JwtBearerTokenValve.kt
@@ -1,6 +1,7 @@
 package no.nav.pensjon.opptjening.afp.api.config.tomcat
 
 import com.nimbusds.jwt.JWTParser
+import net.minidev.json.JSONObject
 import org.apache.catalina.connector.Request
 import org.apache.catalina.connector.Response
 import org.apache.catalina.valves.ValveBase
@@ -11,14 +12,17 @@ class JwtBearerTokenValve : ValveBase() {
     private val logger = getLogger(javaClass)
 
     override fun invoke(request: Request, response: Response) {
-        request.getHeader(HttpHeaders.AUTHORIZATION)?.let {
-            if (it.startsWith(prefix = "bearer", ignoreCase = true)) {
+        request.getHeader(HttpHeaders.AUTHORIZATION)?.let { authHeader ->
+            if (authHeader.startsWith(prefix = "bearer", ignoreCase = true)) {
                 try {
-                    val claimsSet = JWTParser.parse(it.substring("bearer ".length)).jwtClaimsSet
+                    val claimsSet = JWTParser.parse(authHeader.substring("bearer ".length)).jwtClaimsSet
                     request.setAttribute("token_audience", claimsSet.audience)
                     request.setAttribute("token_azp_name", claimsSet.getClaim("azp_name"))
-                    request.setAttribute("token_scope", claimsSet.getClaim("scope"))
+                    (claimsSet.getClaim("consumer") as? JSONObject)
+                        ?.also { request.setAttribute("token_consumer", it["ID"]) }
                     request.setAttribute("token_issuer", claimsSet.issuer)
+                    request.setAttribute("token_roles", claimsSet.getClaim("roles"))
+                    request.setAttribute("token_scope", claimsSet.getClaim("scope"))
                     request.setAttribute("token_subject", claimsSet.subject)
                 } catch (e: RuntimeException) {
                     logger.info("Unable to parse token", e)
